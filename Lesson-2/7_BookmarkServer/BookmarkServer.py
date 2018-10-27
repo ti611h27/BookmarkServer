76fc956091a39d6461175bb67e19723d8eed8494
Innes, Timothy (ti611h) <ti611h@att.com>
2018-10-27 10:33:13 -0400

    My first commit

--- Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -75,8 +75,12 @@ def CheckURI(uri, timeout=5):
     (i.e. times out).
     '''
     # 1. Write this function.  Delete the following line.
-    raise NotImplementedError("Step 1 isn't written yet.")
-
+    #raise NotImplementedError("Step 1 isn't written yet.")
+    try:
+    	r = requests.get(uri, timeout = timeout)
+    	return r.status_code == 200
+    except requests.RequestException:
+        return False
 
 class Shortener(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -88,7 +92,10 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             if name in memory:
                 # 2. Send a 303 redirect to the long URI in memory[name].
                 #    Delete the following line.
-                raise NotImplementedError("Step 2 isn't written yet.")
+                self.send_response(303)
+                self.send_header('Location', memory[name])
+                self.end_headers()
+                #raise NotImplementedError("Step 2 isn't written yet.")
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)
@@ -115,7 +122,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         if "longuri" not in params or "shortname" not in params:
             # 3. Serve a 400 error with a useful message.
             #    Delete the following line.
-            raise NotImplementedError("Step 3 isn't written yet!")
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Missing longuri or shortname".encode())
+            #raise NotImplementedError("Step 3 isn't written yet!")
 
         longuri = params["longuri"][0]
         shortname = params["shortname"][0]
@@ -126,13 +137,20 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
             # 4. Serve a redirect to the root page (the form).
             #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+            #raise NotImplementedError("Step 4 isn't written yet!")
         else:
             # Didn't successfully fetch the long URI.
 
             # 5. Send a 404 error with a useful message.
             #    Delete the following line.
-            raise NotImplementedError("Step 5 isn't written yet!")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Failed to fetch.".encode())
+            #raise NotImplementedError("Step 5 isn't written yet!")
 
 if __name__ == '__main__':
     server_address = ('', 8000)
